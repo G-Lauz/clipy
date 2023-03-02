b979141b454f677547e215a3410e7c25cba08ab1
Gabriel Lauzier <gabriel.lauzier@usherbrooke.ca>
2023-03-02 18:03:08 -0500

    Dev environment setup

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname: str):
 
 setuptools.setup(
     name="clipy",
-    version="0.2.0",
+    version="0.2.1",
     description="A command line interface package.",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
